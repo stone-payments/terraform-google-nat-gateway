46c6c041d7e05af1eaba94abf74df4111917efa1
Gabriel Ferreira <gabidferreira9@gmail.com>
2022-08-31 10:08:36 -0300

    Migrate to terraform major 1.x (#1)
    * Update main.tf
    
    * Fix git repo reference.
    
    * Remove version constraint from module source.
    
    * Update main.tf
    
    * Use mig module with terraform 0.13.x support
    
    * Migrate mig module dependency.
    
    * Upgrade tag version
    
    Signed-off-by: Feggah <gabidferreira9@gmail.com>
    
    Signed-off-by: Feggah <gabidferreira9@gmail.com>
    Co-authored-by: Carlos Natan Queiroz <cnatan@stone.com.br>
    Co-authored-by: cnatan <cnsqueiroz@hotmail.com>

--- main.tf
+++ main.tf
@@ -15,85 +15,137 @@
  */
 
 data "template_file" "nat-startup-script" {
-  template = "${file("${format("%s/config/startup.sh", path.module)}")}"
+  template = file(format("%s/config/startup.sh", path.module))
 
-  vars {
-    squid_enabled = "${var.squid_enabled}"
-    squid_config  = "${var.squid_config}"
-    module_path   = "${path.module}"
+  vars = {
+    squid_enabled = var.squid_enabled
+    squid_config  = var.squid_config
+    module_path   = path.module
   }
 }
 
 data "google_compute_network" "network" {
-  name    = "${var.network}"
-  project = "${var.network_project == "" ? var.project : var.network_project}"
+  name    = var.network
+  project = var.network_project == "" ? var.project : var.network_project
 }
 
 data "google_compute_address" "default" {
-  count   = "${var.ip_address_name == "" ? 0 : 1}"
-  name    = "${var.ip_address_name}"
-  project = "${var.network_project == "" ? var.project : var.network_project}"
-  region  = "${var.region}"
+  count   = var.ip_address_name == "" ? 0 : 1
+  name    = var.ip_address_name
+  project = var.network_project == "" ? var.project : var.network_project
+  region  = var.region
 }
 
 locals {
-  zone          = "${var.zone == "" ? lookup(var.region_params["${var.region}"], "zone") : var.zone}"
+  zone          = var.zone == "" ? lookup(var.region_params[var.region], "zone") : var.zone
   name          = "${var.name}nat-gateway-${local.zone}"
   instance_tags = ["${var.tag_preffix}-inst-${local.zonal_tag}", "${var.tag_preffix}-inst-${local.regional_tag}"]
   zonal_tag     = "${var.name}nat-${local.zone}"
   regional_tag  = "${var.name}nat-${var.region}"
 }
 
-module "nat-gateway" {
-  source                = "GoogleCloudPlatform/managed-instance-group/google"
-  version               = "1.1.15"
-  module_enabled        = "${var.module_enabled}"
-  project               = "${var.project}"
-  region                = "${var.region}"
-  zone                  = "${local.zone}"
-  network               = "${var.network}"
-  subnetwork            = "${var.subnetwork}"
-  subnetwork_project    = "${var.network_project == "" ? var.project : var.network_project}"
-  target_tags           = ["${local.instance_tags}"]
-  instance_labels       = "${var.instance_labels}"
-  service_account_email = "${var.service_account_email}"
-  machine_type          = "${var.machine_type}"
-  name                  = "${local.name}"
-  compute_image         = "${var.compute_image}"
-  size                  = 1
-  network_ip            = "${var.ip}"
+module "instance_template" {
+  source                = "git@github.com:terraform-google-modules/terraform-google-vm.git//modules/instance_template?ref=v7.8.0"
+
+  project_id            = var.project
+  subnetwork            = var.subnetwork
+  subnetwork_project    = var.network_project == "" ? var.project : var.network_project
+  region                = var.region
+  service_account       = { email = var.service_account_email, scopes = []}
+  name_prefix           = local.name
+  tags                  = local.instance_tags
+  labels                = var.instance_labels
   can_ip_forward        = "true"
-  service_port          = "80"
-  service_port_name     = "http"
-  startup_script        = "${data.template_file.nat-startup-script.rendered}"
-  wait_for_instances    = true
-  metadata              = "${var.metadata}"
-  ssh_fw_rule           = "${var.ssh_fw_rule}"
-  ssh_source_ranges     = "${var.ssh_source_ranges}"
-  http_health_check     = "${var.autohealing_enabled}"
+  machine_type          = var.machine_type
+  metadata              = var.metadata
+  startup_script        = data.template_file.nat-startup-script.rendered
+  source_image          = var.compute_image
+  network_ip            = var.ip
+  disk_size_gb          = 10
+  # access_config         = [
+  #   {
+  #     nat_ip = element(concat(google_compute_address.default.*.address, data.google_compute_address.default.*.address, list("")), 0)
+  #   },
+  # ]
+}
 
-  update_strategy = "ROLLING_UPDATE"
+module "nat_gateway_mig" {
+  source                    = "git@github.com:terraform-google-modules/terraform-google-vm.git//modules/mig?ref=v7.8.0"
 
-  rolling_update_policy = [
+  project_id                = var.project
+  region                    = var.region
+  subnetwork                = var.subnetwork
+  subnetwork_project        = var.network_project == "" ? var.project : var.network_project
+  distribution_policy_zones = [local.zone]
+  autoscaling_enabled       = false
+  wait_for_instances        = true
+  hostname                  = local.name
+  instance_template         = module.instance_template.self_link
+  target_size               = 1
+  update_policy             = [
     {
       type                  = "PROACTIVE"
       minimal_action        = "REPLACE"
       max_surge_fixed       = 0
       max_unavailable_fixed = 1
       min_ready_sec         = 30
-    },
-  ]
-
-  access_config = [
-    {
-      nat_ip = "${element(concat(google_compute_address.default.*.address, data.google_compute_address.default.*.address, list("")), 0)}"
+      instance_redistribution_type = "PROACTIVE"
+      max_surge_percent = null
+      max_unavailable_percent = null
+      replacement_method = "RECREATE"
     },
   ]
 }
 
-resource "google_compute_address" "default" {
-  count   = "${var.module_enabled && var.ip_address_name == "" ? 1 : 0}"
-  name    = "${local.zonal_tag}"
-  project = "${var.project}"
-  region  = "${var.region}"
-}
+# module "nat-gateway_dep" {
+#   source                = "git@github.com:stone-payments/terraform-google-managed-instance-group.git?ref=terraform13support"
+#   module_enabled        = var.module_enabled
+#   project               = var.project
+#   region                = var.region
+#   name                  = local.name
+#   network               = var.network
+#   subnetwork            = var.subnetwork
+#   subnetwork_project    = var.network_project == "" ? var.project : var.network_project
+
+#   # zone                  = local.zone  
+#   # wait_for_instances    = true
+#   # service_port          = "80"
+#   # service_port_name     = "http"
+#   # http_health_check     = var.autohealing_enabled
+#   # size                  = 1
+#   # network_ip            = var.ip
+#   # update_strategy = "ROLLING_UPDATE"
+#   # rolling_update_policy = [
+#   #   {
+#   #     type                  = "PROACTIVE"
+#   #     minimal_action        = "REPLACE"
+#   #     max_surge_fixed       = 0
+#   #     max_unavailable_fixed = 1
+#   #     min_ready_sec         = 30
+#   #   },
+#   # ]
+
+#   ssh_fw_rule           = var.ssh_fw_rule
+#   ssh_source_ranges     = var.ssh_source_ranges
+
+#   # compute_image         = var.compute_image
+#   # startup_script        = data.template_file.nat-startup-script.rendered
+#   # metadata              = var.metadata
+#   # machine_type          = var.machine_type
+#   # can_ip_forward        = "true"
+#   # service_account_email = var.service_account_email
+#   # target_tags           = local.instance_tags
+#   # instance_labels       = var.instance_labels
+#   # access_config = [
+#   #   {
+#   #     nat_ip = element(concat(google_compute_address.default.*.address, data.google_compute_address.default.*.address, list("")), 0)
+#   #   },
+#   # ]
+# }
+
+# resource "google_compute_address" "default" {
+#   count   = var.module_enabled && var.ip_address_name == "" ? 1 : 0
+#   name    = local.zonal_tag
+#   project = var.project
+#   region  = var.region
+# }
--- outputs.tf
+++ outputs.tf
@@ -14,32 +14,30 @@
  * limitations under the License.
  */
 
-output depends_id {
-  description = "Value that can be used for intra-module dependency creation."
-  value       = "${module.nat-gateway.depends_id}"
-}
+# output gateway_ip {
+#   description = "The internal IP address of the NAT gateway instance."
+#   value       = module.nat_gateway_mig.self_link.network_ip
+# }
 
-output gateway_ip {
-  description = "The internal IP address of the NAT gateway instance."
-  value       = "${module.nat-gateway.network_ip}"
-}
-
-output instance {
-  description = "The self link to the NAT gateway instance."
-  value       = "${flatten(module.nat-gateway.instances)}"
-}
-
-output external_ip {
-  description = "The external IP address of the NAT gateway instance."
-  value       = "${element(concat(google_compute_address.default.*.address, data.google_compute_address.default.*.address, list("")), 0)}"
-}
+# output instance {
+#   description = "The self link to the NAT gateway instance."
+#   value       = flatten(module.nat_gateway_mig.self_link.instances)
+# }
+# output instance {
+#   description = "The self link to the NAT gateway instance."
+#   value       = "${flatten(module.nat-gateway.instances)}"
+# }
+# output external_ip {
+#   description = "The external IP address of the NAT gateway instance."
+#   value       = "${element(concat(google_compute_address.default.*.address, data.google_compute_address.default.*.address, list("")), 0)}"
+# }
 
 output routing_tag_regional {
   description = "The tag that any other instance will need to have in order to get the regional routing rule"
-  value       = "${local.regional_tag}"
+  value       = local.regional_tag
 }
 
 output routing_tag_zonal {
   description = "The tag that any other instance will need to have in order to get the zonal routing rule"
-  value       = "${local.zonal_tag}"
+  value       = local.zonal_tag
 }
